f85cbc62e214745be59fd633fc56bfc0be97cafb
h-takemoto <ikitsuke2014@gmail.com>
2014-12-31 12:11:25 +0900

    店舗一覧コントローラの店舗取得メソッドのみ作成

--- ims/ikitsuke/src/main/java/jp/co/ikitsuke/controller/ShopManageController.java
+++ ims/ikitsuke/src/main/java/jp/co/ikitsuke/controller/ShopManageController.java
@@ -1,9 +1,16 @@
 package jp.co.ikitsuke.controller;
 
-import jp.co.ikitsuke.form.LoginInputForm;
+import java.util.List;
 
+import jp.co.ikitsuke.form.PurlieuEditOutputForm;
+import jp.co.ikitsuke.logic.ShopInfoLogic;
+import jp.co.ikitsuke.model.ShopInfoModel;
+import jp.co.ikitsuke.utils.ConvertUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,15 +18,24 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ShopManageController {
 
-	@RequestMapping(value = "/shop", method = RequestMethod.GET)
-	public ModelAndView login(@ModelAttribute("LoginInputForm")LoginInputForm loginInputForm) {
+	@Autowired
+	ShopInfoLogic shopInfoLogic;
 
-		loginInputForm.setMailAddress("mailAddress");
-		loginInputForm.setLoginPassword("loginPassword");
+	@RequestMapping(value = "/shopManage/{categoryId}", method = RequestMethod.GET)
+	public ModelAndView login(@PathVariable("categoryId") String categoryId,
+			@ModelAttribute("PurlieuEditOutputForm")PurlieuEditOutputForm purlieuEditOutputForm) {
 
+		//カテゴリーIDで店舗一覧取得
+		List<ShopInfoModel> modelList = shopInfoLogic.getShopInfo(Integer.parseInt(categoryId));
 
-		//ログイン画面を表示
-		return new ModelAndView("login");
+		//一覧を取得した場合
+		if(modelList != null){
+
+			purlieuEditOutputForm.setShopInfoList(ConvertUtil.toShopInfoParts(modelList));
+		}
+
+		//店舗一覧画面を表示
+		return new ModelAndView("shopList","PurlieuEditOutputForm",purlieuEditOutputForm);
 	}
 
 }
